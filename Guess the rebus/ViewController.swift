65c2ac2915ce4a979422f4b9c0ce5630795fefb4
Evgen-ios <jack38irk@icloud.com>
2021-04-07 12:06:18 +0800

    Initial Commit

---
+++ Guess the rebus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guess the rebus
+//
+//  Created by Evgeniy Goncharov on 07.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
